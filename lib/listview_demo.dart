5ecc98277b2728a2b46a6bd0a0432fb48d416bc1
khaledhosni <meckawy@gmail.com>
2023-09-25 11:12:13 +0300

    list.builder() has been added

--- lib/listview_demo.dart
+++ lib/listview_demo.dart
@@ -1,8 +1,21 @@
+import 'package:day3_ui/model/movie.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
+  var movieList=[
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+    Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion", 1994, "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/Tim-Robbins-as-Andy-Dufresne-in-Shawshank-Redemption.jpg"),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +26,7 @@ class MyApp extends StatelessWidget {
         ),
         body:
           Container(
-            child: createListView(),
+            child: createDynamicList(movieList),
           ),
 
       ),
@@ -61,6 +74,50 @@ class MyApp extends StatelessWidget {
     );
 
   }
+
+  
+  
+   createDynamicList(List<Movie> movieList){
+
+     return ListView.builder(
+        shrinkWrap: true,
+         itemCount: movieList.length,
+         itemBuilder:(context, index) {
+
+           return Card(
+             child: InkWell(
+               onTap: (){
+
+                 print(movieList[index].name);
+
+               },
+               child: Row(
+                 mainAxisSize: MainAxisSize.max,
+                 children: [
+                   Image.network(movieList[index].photoURL, width: 100,height: 100,fit:BoxFit.cover,),
+                   Expanded(
+                     child: Column(
+                       mainAxisSize: MainAxisSize.max,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Container(
+                             margin: EdgeInsets.all(10),
+                             child: Text(movieList[index].name, style: TextStyle(fontSize: 20), textAlign: TextAlign.start ,)),
+                         Container(
+                             margin: EdgeInsets.all(10),
+                             child: Text(movieList[index].description, maxLines: 10)),
+                       ],
+                     ),
+                   )
+                 ],
+               ),
+             ),
+           );
+         },
+     );
+    
+   } 
+
 }
 
 
---
+++ lib/model/movie.dart
@@ -0,0 +1,9 @@
+
+class Movie{
+  var name;
+  var description;
+  var year;
+  var photoURL;
+
+  Movie(this.name, this.description, this.year, this.photoURL);
+}
